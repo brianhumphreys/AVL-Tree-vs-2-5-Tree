2605691a56dbed8dbe596150a1cfdd68bd606c21
Brian Humphreys <brianhumphreys@umail.ucsb.edu>
2018-02-28 23:11:57 -0800

    file set up

---
+++ AVL.cpp
@@ -0,0 +1,9 @@
+#include <string>
+
+using namespace std;
+
+class AVL {
+
+BST::
+
+}
---
+++ AVL.h
@@ -0,0 +1,23 @@
+#ifndef __AVL_H_INCLUDED__
+#define __AVL_H_INCLUDED__
+
+#include <string>
+
+using namespace std;
+
+class AVL {
+
+ private:
+
+ 	struct Node
+ 	{
+ 		int value;
+ 		Node* right;
+ 		Node* left;
+
+ 	};
+
+ public:
+
+
+}
